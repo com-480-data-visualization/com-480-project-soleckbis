6252d0bc07f0d92020f2dde6bc6af7ad4d5c0c43
Lucas Eckes <lucas.eckes@epfl.ch>
2020-05-01 23:55:47 +0200

    add hover

--- concentration.js
+++ concentration.js
@@ -1,34 +1,58 @@
 
-class MapCircle {
+class MapPlot {
 	
-	makeCirclebar(svg, color_scale, top_left, colorbar_size, scaleClass=d3.scaleLinear) {
+	makeColorbar(svg, color_scale, top_left, colorbar_size, scaleClass=d3.scaleLinear) {
 
-		const value_to_svg = d3.scalePoint()
-			.domain([1, 10, 100])
+		const value_to_svg = scaleClass()
+			.domain(color_scale.domain())
 			.range([colorbar_size[1], 0]);
-		
-		const range01_to_radius = d3.scaleLinear()
-			.domain([0, 100])
-			.range([0, 400]);
+
+		const range01_to_color = d3.scaleLinear()
+			.domain([0, 1])
+			.range(color_scale.range())
+			.interpolate(color_scale.interpolate());
 
 		// Axis numbers
 		const colorbar_axis = d3.axisLeft(value_to_svg)
-			.tickFormat(d3.format(".0f"))
+			.tickFormat(d3.format(".0e"))
 
 		const colorbar_g = this.svg.append("g")
 			.attr("id", "colorbar")
 			.attr("transform", "translate(" + top_left[0] + ', ' + top_left[1] + ")")
-			.call(colorbar_axis)
-			.selectAll('.tick')
-				.append("circle")
-				.attr('r', (d)=>Math.sqrt(range01_to_radius(d)))
-				.attr("cx", 50)
-				.style("fill", "red");
+			.call(colorbar_axis);
 
-
-			
-			
+		// Create the gradient
+		function range01(steps) {
+			return Array.from(Array(steps), (elem, index) => index / (steps-1));
 		}
+
+		const svg_defs = this.svg.append("defs");
+
+		const gradient = svg_defs.append('linearGradient')
+			.attr('id', 'colorbar-gradient')
+			.attr('x1', '0%') // bottom
+			.attr('y1', '100%')
+			.attr('x2', '0%') // to top
+			.attr('y2', '0%')
+			.attr('spreadMethod', 'pad');
+
+		gradient.selectAll('stop')
+			.data(range01(10))
+			.enter()
+			.append('stop')
+				.attr('offset', d => Math.round(100*d) + '%')
+				.attr('stop-color', d => range01_to_color(d))
+				.attr('stop-opacity', 1);
+
+		// create the colorful rect
+		colorbar_g.append('rect')
+			.attr('id', 'colorbar-area')
+			.attr('width', colorbar_size[0])
+			.attr('height', colorbar_size[1])
+			.style('fill', 'url(#colorbar-gradient)')
+			.style('stroke', 'black')
+			.style('stroke-width', '1px')
+	}
 	
 	constructor(svg_element_id){
 		this.svg = d3.select('#' + svg_element_id);
@@ -46,15 +70,16 @@ class MapCircle {
 		const path_generator = d3.geoPath()
 			.projection(projection);
 			
-		const radius_scale = d3.scaleLinear()
-			.range([0, 400]);
+		const color_scale = d3.scaleLinear()
+			.range(["rgb(255,237,160)", "rgb(240,59,32)"])
+			.interpolate(d3.interpolateHcl);
 			
 		const disease_promise = d3.csv("data/test.csv").then((data)=>{
-			let province_daily = {};
+			let province_concentration = {};
 			data.forEach((row)=> {
-				province_daily[row.province] = parseFloat(row.province_cases);
+				province_concentration[row.province] = parseFloat(row.proportion_province);
 			});
-			return province_daily;
+			return province_concentration;
 		});
 			
 		const map_promise = d3.json('json/skorea-provinces-topo.json').then((topojson_raw)=> {
@@ -65,7 +90,8 @@ class MapCircle {
 		function handleMouseOver(d, i) {
 			var coords = d3.mouse(this)
 			var svg = d3.select('#' + svg_element_id);
-			d3.select(this).style('stroke-width', 1)
+			d3.select(this).style('fill-opacity', 1)
+			.style('stroke-width', 1)
 			d3.select('#' + svg_element_id).append('text')
 				.attr("id", "t"+d.properties.NAME_1)
 				.attr("x", coords[0]-30)
@@ -74,7 +100,8 @@ class MapCircle {
 		}
 		
 		function handleMouseOut(d, i) {
-			d3.selectAll('.province').style('stoke-width', 0.2)
+			d3.selectAll('.province').style('fill-opacity', 0.6)
+			.style('stoke-width', 0.2)
 			d3.select("#t"+d.properties.NAME_1).remove();
 		}
 		
@@ -83,15 +110,14 @@ class MapCircle {
 			let province_disease = results[1];
 			
 			map_data.forEach(province => {
-				province.properties.cases = province_disease[province.properties.NAME_1]
+				province.properties.density = province_disease[province.properties.NAME_1]
 			})
 			
-			const cases = Object.values(province_disease);
+			const concentration = Object.values(province_disease);
 			
-			radius_scale.domain([0, 100]);
+			color_scale.domain([d3.min(concentration), d3.max(concentration)]);
 			
 			this.map_container = this.svg.append('g');
-			this.circle_container = this.svg.append('g');
 			
 			this.map_container.selectAll(".province")
 				.data(map_data)
@@ -99,22 +125,11 @@ class MapCircle {
 				.append("path")
 				.classed("province", true)
 				.attr("d", path_generator)
-				.style("fill", 'white')
-				.on('mouseover', handleMouseOver)
+				.style("fill", (d)=> color_scale(d.properties.density))
+				.on('mouseover', handleMouseOver) 
 				.on('mouseout', handleMouseOut);
-
-				
-				
-			this.circle_container.selectAll(".province-circles")
-				.data(map_data)
-				.enter()
-				.append("circle")
-				.classed("province-circles", true)
-				.attr("r", (d)=>Math.sqrt(radius_scale(d.properties.cases)))
-				.attr("transform", (d)=> "translate("+path_generator.centroid(d)+")")
-				.style("fill", "red");
-				
-			this.makeCirclebar(this.svg, radius_scale, [50, 30], [20, this.svg_height - 2*30]);
+			
+			this.makeColorbar(this.svg, color_scale, [50, 30], [20, this.svg_height - 2*30]);
 		})
 	};
 }
@@ -129,7 +144,7 @@ function whenDocumentLoaded(action) {
 }
 
 whenDocumentLoaded(() => {
-	plot_object = new MapCircle('circles');
+	plot_object = new MapPlot('concentration');
 	// plot object is global, you can inspect it in the dev-console
 });
 
