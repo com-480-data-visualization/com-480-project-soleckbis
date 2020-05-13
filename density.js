7738e3d8323769ee5217c346be0a6150dc7f8090
antonsold <soldatenkovanton@mail.ru>
2020-05-13 23:19:30 +0200

    made radiobutton work

--- density.js
+++ density.js
@@ -54,10 +54,41 @@ class DensityMapPlot {
             .style('stroke-width', '1px')
     }
 
-    constructor(svg_element_id, feature) {
+    fill(feature, path_generator, scale, class_) {
+        this.svg.select("defs").remove();
+        this.svg.select("#colorbar").remove();
+        this.svg.empty();
+        const maxValue = Math.max(...this.map_data.map(o => o.properties[feature]), 0);
+        const minValue = Math.min(...this.map_data.map(o => o.properties[feature]), 1e6);
+        scale.domain([minValue, maxValue]);
+        console.log(scale(5000));
         let tooltip = d3.select('body').append('div')
             .attr('class', 'hidden tooltip');
-        console.log(tooltip);
+        this.map_container.selectAll(".province").data(this.map_data)
+            .enter()
+            .append("path")
+            .classed("province", true)
+            .attr("d", path_generator)
+            .style("fill", (d) => scale(d.properties[feature]))
+            .style("stroke-width", 0.2)
+            .on('mouseover', function(d) {
+                d3.select(this).style("stroke-width", 1);
+                let mouse = d3.mouse(this).map(function(d) {
+                    return parseInt(d);
+                });
+                tooltip.classed('hidden', false)
+                    .attr('style', 'left:' + (mouse[0] + 100) +
+                        'px; top:' + (mouse[1] + 1700) + 'px')
+                    .html(d.properties.NAME_1 + ": " + d.properties[feature]);
+            })
+            .on('mouseout', function() {
+                d3.select(this).style("stroke-width", 0.2);
+                tooltip.classed('hidden', true);
+            });
+        this.makeColorbar(this.svg, scale, [50, 30], [20, this.svg_height - 2*30], class_);
+    }
+
+    constructor(svg_element_id, feature, scale=d3.scaleLog().range(["hsl(62,100%,90%)", "hsl(228,30%,20%)"]).interpolate(d3.interpolateHcl)) {
         this.svg = d3.select('#' + svg_element_id);
         const svg_viewbox = this.svg.node().viewBox.animVal;
         this.svg_width = svg_viewbox.width;
@@ -68,17 +99,12 @@ class DensityMapPlot {
             .scale(4000)
             .translate([this.svg_width / 2, this.svg_height / 2]) // SVG space
             .precision(.1);
-        // may be useful for calculating scales
-        const path_generator = d3.geoPath()
+        this.path_generator = d3.geoPath()
             .projection(projection);
-        //colormap for population density
-        this.color_scale = d3.scaleLog()
-            .range(["hsl(62,100%,90%)", "hsl(228,30%,20%)"])
-            .interpolate(d3.interpolateHcl);
 
-        const population_promise = d3.csv("data/convertcsv.csv").then((data) => {
+        const population_promise = d3.csv("data/agg.csv").then((data) => {
             return data.reduce((acc, d) => {
-                acc[d.name] = d;
+                acc[d.province] = d;
                 return acc;
             }, {});
         });
@@ -88,49 +114,20 @@ class DensityMapPlot {
             return canton_paths.features;
         });
 
-        const agg_promise = d3.csv("data/agg.csv").then((data)=> {
-            return data.reduce((acc, d) => {
-                acc[d.province] = d;
-                return acc;
-            })
-        });
 
-
-        Promise.all([map_promise, population_promise, agg_promise]).then((results) => {
-            let map_data = results[0];
+        Promise.all([map_promise, population_promise]).then((results) => {
+            this.map_data = results[0];
             let stats = results[1];
-            let province_agg = results[2];
             //let point_data = results[2];
             this.map_container = this.svg.append('g');
             //this.point_container = this.svg.append('g');
             //this.label_container = this.svg.append('g');
-            map_data.forEach((province) => {
-                province.properties.density = parseFloat(stats[province.properties.NAME_1].density);
+            console.log(stats);
+            this.map_data.forEach((province) => {
+                province.properties.density = parseFloat(stats[province.properties.NAME_1].population_density);
+                province.properties.schools = parseFloat(stats[province.properties.NAME_1].elementary_school_count);
             });
-            this.color_scale.domain([75, 12000]);
-            this.map_container.selectAll(".province").data(map_data)
-                .enter()
-                .append("path")
-                .classed("province", true)
-                .attr("d", path_generator)
-                .style("fill", (d) => this.color_scale(d.properties.density))
-                .style("stroke-width", 0.2)
-                .on('mousemove', function(d) {
-                    d3.select(this).style("stroke-width", 1);
-                    let mouse = d3.mouse(this).map(function(d) {
-                        return parseInt(d);
-                    });
-                    tooltip.classed('hidden', false)
-                        .attr('style', 'left:' + (mouse[0] + 100) +
-                            'px; top:' + (mouse[1] + 3200) + 'px')
-                        .html(d.properties.NAME_1 + ": " + d.properties.density);
-                })
-                .on('mouseout', function() {
-                    d3.select(this).style("stroke-width", 0.2);
-                    tooltip.classed('hidden', true);
-                });
-                //.text((d) => d.properties.NAME_1 + ": " + d.properties.density);
-            this.makeColorbar(this.svg, this.color_scale, [50, 30], [20, this.svg_height - 2*30]);
+            this.fill(feature, this.path_generator, scale);
         });
     }
 }
@@ -142,8 +139,15 @@ whenDocumentLoaded(() => {
     feature_selector.forEach((input) => {
         input.onclick = (() => {
             feature_ = input.value;
-            d3.select('density_map').remove();
-            densityMap = new DensityMapPlot('density_map', feature_);
+            let scale = d3.scaleLinear().range(["hsl(62,100%,90%)", "hsl(228,30%,20%)"]).interpolate(d3.interpolateHcl);
+            let class_ = d3.scaleLinear;
+            if(feature_ === "density"){
+                scale = d3.scaleLog().range(["hsl(62,100%,90%)", "hsl(228,30%,20%)"]).interpolate(d3.interpolateHcl);
+                class_ = d3.scaleLog;
+            }
+            //console.log(scale.)
+            densityMap.map_container.selectAll("*").remove();
+            densityMap.fill(feature_, densityMap.path_generator, scale);
         });
     });
 });
