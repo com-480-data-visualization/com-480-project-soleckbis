3f0a4390b68f34528fd73aca8c058d843061db37
bichoff <frederic.bischoff@epfl.ch>
2020-05-19 18:44:09 +0200

    figure 1 weather finished

--- final/js/weather_1.js
+++ final/js/weather_1.js
@@ -1,5 +1,6 @@
 
-
+	function get_graph(name,color,unit){
+	
 	// set the dimensions and margins of the graph
 	var margin = {top: 10, right:0, bottom: 30, left:0},
 	    width = 1050 - margin.left - margin.right,
@@ -7,6 +8,7 @@
 	
 	var width_score = 150
 	var width_s = width -width_score
+	
 	// append the svg object to the body of the page
 	var svg = d3.select("#my_dataviz")
 	  .append("svg")
@@ -15,16 +17,48 @@
 	  .append("g")
 	    .attr("transform",
 	          "translate(" + margin.left + "," + margin.top + ")");
-
-
+	
 	d3.csv("data/sick_meteo.csv", function(row) {
-	  return {date: row.date, count: row.count, avg_temp: row.avg_temp}
+	  return {date: row.date, count: row.count, 
+	  		  avg_temp: row.avg_temp,
+	  		  avg_hum: row.avg_relative_humidity,
+	  		  avg_rain:row.precipitation,
+	  		  avg_wind:row.max_wind_speed
+	  		  }
 	}).then(function(d){
 
-	var avg_temp = d3.nest()
-  	.key(function(d1) {return d1.date; })
-  	.rollup(function(v) { return d3.mean(v, function(d1) { return d1.avg_temp}); })
-  	.entries(d);
+	var avg_feat = d3.nest()
+	  	.key(function(d1) {return d1.date; })
+	  	.rollup(function(v) { return d3.mean(v, function(d1) { return d1.avg_temp}); })
+	  	.entries(d);
+
+	if (name == 'Temperature'){
+		avg_feat = d3.nest()
+	  	.key(function(d1) {return d1.date; })
+	  	.rollup(function(v) { return d3.mean(v, function(d1) { return d1.avg_temp}); })
+	  	.entries(d);
+  	}
+  	
+  	else if(name == 'Humidity'){
+		avg_feat = d3.nest()
+	  	.key(function(d1) {return d1.date; })
+	  	.rollup(function(v) { return d3.mean(v, function(d1) { return d1.avg_hum}); })
+	  	.entries(d);
+  	}
+  	
+  	else if(name == 'Rain'){
+		 avg_feat = d3.nest()
+	  	.key(function(d1) {return d1.date; })
+	  	.rollup(function(v) { return d3.mean(v, function(d1) { return d1.avg_rain}); })
+	  	.entries(d);
+  	}
+  	
+  	else if(name == 'Wind'){
+		avg_feat = d3.nest()
+	  	.key(function(d1) {return d1.date; })
+	  	.rollup(function(v) { return d3.mean(v, function(d1) { return d1.avg_wind}); })
+	  	.entries(d);
+  	}
   	
   	var nb_patient = d3.nest()
   	.key(function(d1) {return d1.date; })
@@ -34,7 +68,7 @@
    	var mapped_count_0 = nb_patient.map(d => {
   	return {date: new Date(d.key),value: d.value,rayon:3}});
 
-	var mapped_temp_0 = avg_temp.map(d => {
+	var mapped_feat_0 = avg_feat.map(d => {
   	return {date: new Date(d.key),value: d.value,rayon:3}});
   	
 	function reduce_left(mapped,nb) { 
@@ -79,23 +113,23 @@
 		}
   	 	
   	var mapped_count = reduce_left(mapped_count_0,0)
-	var mapped_temp = reduce_right(mapped_temp_0,0)
+	var mapped_feat = reduce_right(mapped_feat_0,0)
 	
 	
   	var count = Object.keys(mapped_count).map(e => parseFloat(mapped_count[e].value));
-	var temp = Object.keys(mapped_temp).map(e => parseFloat(mapped_temp[e].value));
+	var feat = Object.keys(mapped_feat).map(e => parseFloat(mapped_feat[e].value));
 	
-	var R = pcorr(count,temp);
+	var R = pcorr(count,feat);
 	var R_mapped = [{'value':Math.round(R*100)/100}];
 	
 	var minDate_1 = get_min_date(mapped_count);
   	var maxDate_1 = get_max_date(mapped_count);
   
-  	var minDate_2 = get_min_date(mapped_temp);
-  	var maxDate_2 = get_max_date(mapped_temp);
+  	var minDate_2 = get_min_date(mapped_feat);
+  	var maxDate_2 = get_max_date(mapped_feat);
   
-  	var minTemp = get_min_value(mapped_temp);
-  	var maxTemp = get_max_value(mapped_temp);
+  	var minFeat = get_min_value(mapped_feat);
+  	var maxFeat = get_max_value(mapped_feat);
   	
   	var minCount = get_min_value(mapped_count);
   	var maxCount= get_max_value(mapped_count);
@@ -121,7 +155,7 @@
     .range([height,0])
   	
   	var yScale2 = d3.scaleLinear().
-  	domain([minTemp,maxTemp])
+  	domain([minFeat,maxFeat])
   	.range([height, 0]);
   	
   	var yScale3 = d3.scaleLinear().
@@ -144,8 +178,6 @@
 		.ticks(10) 
 		
    var svgContainer = d3.select("body").append("svg");
-  
-  var textSpec = [{"content" : "testing", "font-family" : "sans-serif","font-size" : "80px", "color" : "red" , "x" : 100 , "y" : 15}];
   
 // Y axis correlation
 	svg.append('g').data(R_mapped)
@@ -171,17 +203,17 @@ var pos = height +10
  		 .attr("font-family", "century")
       .attr('class','axis')
       .attr('transform', 'translate(0,' + pos + ')')
-  	  .attr("stroke","blue")
+  	  .attr("stroke","#0D47A1")
   	   
  x_axis_real2 =svg.append('g')
       .attr('class','axis')
       .attr('transform', 'translate(0,-10)')
-      .attr("stroke","red")
+      .attr("stroke",color)
       
   // Y-axis
   svg.append('g').data(mapped_count)
       .attr('class', 'axis')
-       .attr("stroke","blue")
+       .attr("stroke","#0D47A1")
       .call(yAxis1)
       .append('text') // y-axis Label
       .attr('class','label')
@@ -194,22 +226,22 @@ var pos = height +10
       .text('Nb new cases')
   	  .style("text-decoration", "oblique")
   	  
-  svg.append('g').data(mapped_temp)
+  svg.append('g').data(mapped_feat)
       .attr('class', 'axis')
-       .attr("stroke","red") 
+       .attr("stroke",color) 
       .attr('transform','translate('+ width_s +',0)')    
       .call(yAxis2)
       .append('text') // y-axis Label
       .attr('class','label')
       .attr('transform','rotate(-90)')
-      .attr("stroke","red")
+      .attr("stroke",color)
       .attr('x',-height/2 + 70)
       .attr('y',+25)
       .attr('dy','.71em')
       .style('text-anchor','end')
       .style("font", "15px times")
-      .text('Weather feature')
-  	  .style("fill", "red")
+      .text(name+ ' ('+unit+')')
+  	  .style("fill", color)
        
    var formatTime = d3.timeFormat("%B %d, %Y")
                     
@@ -226,24 +258,18 @@ var pos = height +10
 	      .attr('cy',function (d) { return yScale3(d.value) })
 	      .attr('r','5')
 	      .attr('stroke','')
-	      .attr('fill','orange')
-		  .attr('stroke','red')
+	      .attr('fill','#ffb142')
+		  .attr('stroke','#1B1464')
 	
 	var mapped_init_1 = initialize_circles(mapped_count);
 	
-	var mapped_init_2 = initialize_circles(mapped_temp);
+	var mapped_init_2 = initialize_circles(mapped_feat);
 	
 	// when the input range changes update the circle 
 	d3.select("#time_lag").on("input", function() {
 	  update(+this.value);
 	});
-	
-	/*
-	essai = [{date:40,value:40},{date:180,value:80},{date:300,value:300},{date:40,value:40},
-	{date:40,value:40},{date:180,value:80},{date:300,value:300},{date:40,value:40}
-	]*/
-	
-	
+		
 	var circles_count= svg.selectAll('circle_count')
 	      .data(mapped_init_1)
 	      .enter()
@@ -254,7 +280,7 @@ var pos = height +10
 	      .attr('stroke','')
 	      .attr('fill','orange')
 	
-	var circles_temp= svg.selectAll('circle_temp')
+	var circles_feat= svg.selectAll('circle_feat')
 	      .data(mapped_init_2)
 	      .enter()
 	      .append('circle')
@@ -281,16 +307,16 @@ function update(time_lag) {
 	
   mapped_count = reduce_left(mapped_count_0,time_lag)
   mapped_count_clean = clean_left(mapped_count_0,time_lag)
-  mapped_temp = reduce_right(mapped_temp_0,time_lag)
-  mapped_temp_clean = clean_right(mapped_temp_0,time_lag)
+  mapped_feat = reduce_right(mapped_feat_0,time_lag)
+  mapped_feat_clean = clean_right(mapped_feat_0,time_lag)
   
   count = Object.keys(mapped_count).map(e => parseFloat(mapped_count[e].value));
-  temp = Object.keys(mapped_temp).map(e => parseFloat(mapped_temp[e].value));
+  feat = Object.keys(mapped_feat).map(e => parseFloat(mapped_feat[e].value));
 
   console.log('time_lag: ',time_lag)
 
   
-  R = pcorr(count,temp);
+  R = pcorr(count,feat);
   R_mapped = [{'value':Math.round(R*100)/100}]
 
 	
@@ -316,8 +342,8 @@ function update(time_lag) {
   minDate_1 = get_min_date(mapped_count);
   maxDate_1 = get_max_date(mapped_count);
   
-  minDate_2 = get_min_date(mapped_temp);
-  maxDate_2 = get_max_date(mapped_temp);
+  minDate_2 = get_min_date(mapped_feat);
+  maxDate_2 = get_max_date(mapped_feat);
   
   xScale1.domain([minDate_1, maxDate_1])
   xScale2.domain([minDate_2, maxDate_2])
@@ -341,54 +367,55 @@ function update(time_lag) {
       .attr('x',width_s/2 +75 )
       .style('text-anchor','end')
       .style("font", "18px times")
-  	  .style("fill", "red")
+  	  .style("fill", color)
   	  
    path1.datum(mapped_count)
 	      .attr("fill", "none")
-	      .attr("stroke", "blue")
+	      .attr("stroke", "#0D47A1")
 	      .attr("stroke-width", 1.5)
 	      .attr("d", d3.line()
 	        .x(function(d) { return xScale1(d.date) })
 	        .y(function(d) { return yScale1(d.value)  })
 	        )
 
-   path2.datum(mapped_temp)
+   path2.datum(mapped_feat)
 	      .attr("fill", "none")
-	      .attr("stroke", "red")
+	      .attr("stroke", color)
 	      .attr("stroke-width", 1.5)
 	      .attr("d", d3.line()
 	        .x(function(d) { return xScale2(d.date) })
 	        .y(function(d) { return yScale2(d.value)  })
 	        )
 	
-	var date_end_temp = mapped_temp[0]
+	var date_end_feat = mapped_feat[2]
 	var date_end_count = mapped_count[mapped_count.length -1]
 	
-	label1.attr("transform", function(d) { return "translate(" + xScale2(date_end_temp.date) + "," + yScale2(date_end_temp.value) + ")"; }) // Put the text at the position of the last point
-          .attr("x", +10) 
-          .attr("y", +10) 
-          .text(function(d) {return 'weather feature'; })
-          .style("fill", 'red')
-          .style("font-size", 10)
+	label1.attr("transform", function(d) { return "translate(" + xScale2(date_end_feat.date) + "," + yScale2(date_end_feat.value) + ")"; }) // Put the text at the position of the last point
+          .attr("x",-20) 
+          .attr("y", -15) 
+          .text(function(d) {return name; })
+          .style("fill", color)
+          .style("font-size", 12)
           
     label2.attr("transform", function(d) { return "translate(" + xScale1(date_end_count.date) + "," + yScale1(date_end_count.value) + ")"; }) // Put the text at the position of the last point
-          .attr("x", -70)
+          .attr("x", -80)
+          .attr("y", +5)
           .text(function(d) {return 'nb new cases'; })
-          .style("fill", 'blue')
-          .style("font-size", 10)
+          .style("fill", '#0D47A1')
+          .style("font-size", 12)
           
 	circles_count 
       .data(mapped_count_clean)
       .attr('cx', function (d) { return xScale1(d.date) })
       .attr('cy', function (d) { return yScale1(d.value) })
       .attr('r', function (d) { return d.rayon })
-      .attr('stroke','blue')
-      .attr('fill','blue')
+      .attr('stroke','#0D47A1')
+      .attr('fill','#0D47A1')
       .on('mouseover', function () {
         d3.select(this)
           .transition()
           .duration(500)
-          .attr('r',10)
+          .attr('r',8)
           .attr('stroke-width',4)
       })
       .on('mouseout', function () {
@@ -399,21 +426,21 @@ function update(time_lag) {
           .attr('stroke-width',1)
       })
       .append('title') // Tooltip
-      .text(function (d) { return '\nNew cases: ' + d.value +
-                           '\nDate: ' + formatTime(new Date(d.date))})
+      .text(function (d) { return '\nNb new cases: ' + d.value +
+                           '\nDay: ' + formatTime(new Date(d.date))})
 	
-	circles_temp 
-      .data(mapped_temp_clean)
+	circles_feat 
+      .data(mapped_feat_clean)
       .attr('cx', function (d) { return xScale2(d.date) })
       .attr('cy', function (d) { return yScale2(d.value) })
       .attr('r', function (d) { return d.rayon })
-      .attr('stroke','red')
-      .attr('fill','red')
+      .attr('stroke',color)
+      .attr('fill',color)
 	  .on('mouseover', function () {
         d3.select(this)
           .transition()
           .duration(500)
-          .attr('r',10)
+          .attr('r',8)
           .attr('stroke-width',4)
       })
       .on('mouseout', function () {
@@ -424,22 +451,46 @@ function update(time_lag) {
           .attr('stroke-width',1)
       })
       .append('title') // Tooltip
-      .text(function (d) { return '\nAvg_Temp: ' + Math.round(d.value*10)/10 +'\ deg. C. '+
-                           '\nDate: ' + formatTime(new Date(d.date))}) 
+      .text(function (d) { return '\n'+name+': ' + Math.round(d.value*10)/10 +'\ '+unit +
+                           '\nDay: ' + formatTime(new Date(d.date))}) 
 	
 	 
 }
     
 update(0)
-    
- d3.selectAll("text")
-  .style('font-family', 'Harman Simple')
-    
-               
+d3.selectAll("text")
+  .style('font-family', 'Harman Simple')   
+
+              
 // Initial starting radius of the circle 
 
 });
 
+}
+
+get_graph('Temperature','red','°C')
+
+function update1(a) {
+	
+	if (a == 'Temp') {
+		d3.select("#my_dataviz").selectAll("svg").remove();
+    	get_graph('Temperature','red','°C')
+  		
+  	} else if (a == 'Hum') {
+  		d3.select("#my_dataviz").selectAll("svg").remove();
+    	get_graph('Humidity','orange','%')	
+	}
+	else if (a == 'Wind') {
+    	d3.select("#my_dataviz").selectAll("svg").remove();
+    	get_graph('Wind','green','%')
+	}
+	else if (a == 'Rain') {
+    	d3.select("#my_dataviz").selectAll("svg").remove();
+    	get_graph('Rain','grey','%')
+	}
+	
+	
+}
 function get_min_date(mapped) {
   return d3.min(mapped, function(d) { return d.date; });
 }
